a030db5834f3e2d82e121acc00d02dd725f49708
marcusruas <marcusruas.andrade@gmail.com>
2022-05-16 14:25:47 -0300

    Corrigido formatação

--- Api/Program.cs
+++ Api/Program.cs
@@ -13,37 +13,35 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(config => config.AdicionarConfiguracoes());
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(cnf =>
+builder.Services.AddSwaggerGen(cnf => {
+    cnf.SwaggerDoc(VERSAO_API, new OpenApiInfo { Version = VERSAO_API, Title = NOME_API });
+    cnf.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        cnf.SwaggerDoc(VERSAO_API, new OpenApiInfo { Version = VERSAO_API, Title = NOME_API });
-        cnf.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            Description = @"Header autenticacao via Json Web Tokens (JWT). insira abaixo o seu token da seguinte forma: 'Bearer 12345abcdef'",
-            Name = "Authorization",
-            In = ParameterLocation.Header,
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer"
-        });
+        Description = @"Header autenticacao via Json Web Tokens (JWT). insira abaixo o seu token da seguinte forma: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
 
-        cnf.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+    cnf.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+    {
+        new OpenApiSecurityScheme
         {
-            new OpenApiSecurityScheme
+        Reference = new OpenApiReference
             {
-            Reference = new OpenApiReference
-                {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-                },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header,
-
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
             },
-            new List<string>()
-            }
-        });
-    }
-);
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+        },
+        new List<string>()
+        }
+    });
+});
 
 builder.Services.AdicionarMensageria();
 builder.Services.AdicionarAutenticacao();
